37199acc9acd91d64bdb960c6d2782f8697920da
Arron Woods <aw@chartblocks.com>
2014-10-13 12:37:08 +0100

    Removed dev API url

--- config.php
+++ config.php
@@ -2,7 +2,6 @@
 
 return array(
     'client' => array(
-        'api_url' => 'http://192.168.100.100/data-server/',
         'token' => '541aa123279871be048b4567',
         'secret' => '31951bc93d11414144f700e279e1ab45'
     ),
